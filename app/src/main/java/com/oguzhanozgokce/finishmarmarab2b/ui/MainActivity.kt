69ee2a7608010a3c354da13413fbd1218972c442
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2024-11-25 18:35:30 +0300

    activity de navigation was established

--- app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ui/MainActivity.kt
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ui/MainActivity.kt
@@ -4,32 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.oguzhanozgokce.finishmarmarab2b.ui.navigation.NavigationGraph
-import com.oguzhanozgokce.finishmarmarab2b.ui.theme.MB2BTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.oguzhanozgokce.finishmarmarab2b.navigation.FMBottomBar
+import com.oguzhanozgokce.finishmarmarab2b.navigation.NavigationGraph
+import com.oguzhanozgokce.finishmarmarab2b.navigation.Screen
+import com.oguzhanozgokce.finishmarmarab2b.navigation.getRoute
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.LMTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-            MB2BTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-                    val startDestination = "Welcome"
-                    NavigationGraph(
+            LMTheme {
+                val navController = rememberNavController()
+                val visibleBottomScreens = listOf(
+                    Screen.Home.getRoute(),
+                    Screen.Favorite.getRoute(),
+                    Screen.Cart.getRoute(),
+                    Screen.Profile.getRoute()
+                )
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppContent(
                         navController = navController,
-                        startDestination = startDestination,
-                        modifier = Modifier.padding(innerPadding)
+                        visibleBottomScreens = visibleBottomScreens
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun AppContent(
+    navController: NavHostController,
+    visibleBottomScreens: List<String>
+) {
+    val bottomBarVisible = navController
+        .currentBackStackEntryAsState()
+        .value?.destination?.route in visibleBottomScreens
+
+    Scaffold(
+        bottomBar = {
+            if (bottomBarVisible) {
+                FMBottomBar(
+                    navController = navController,
+                    onNavigateToDestination = { destination ->
+                        navController.navigate(destination.screen.getRoute()) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+    ) { innerPadding ->
+        NavigationGraph(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }

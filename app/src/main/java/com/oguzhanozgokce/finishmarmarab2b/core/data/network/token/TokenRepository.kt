e7868639a77c6ca7ffeae29edf647a276f63755a
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2024-12-17 00:31:01 +0300

    remove files

--- app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/core/data/network/token/TokenRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.oguzhanozgokce.finishmarmarab2b.core.data.network.token
-
-interface TokenRepository {
-    suspend fun getRefreshToken(): String?
-    suspend fun refreshAccessToken(): String?
-    fun clearAllTokens()
-}
--- app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ecommerce/data/source/remote/response/TokenResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.oguzhanozgokce.finishmarmarab2b.ecommerce.data.source.remote.response
-
-data class TokenResponse(
-    val accessToken: String,
-    val refreshToken: String,
-    val expirationTime: Long
-)

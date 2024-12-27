f25f5a76e190845fd784823d16e024bdc4f4641d
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2024-12-27 19:28:33 +0300

    Api routes object createdd

---
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/core/common/ApiRoutes.kt
@@ -0,0 +1,7 @@
+package com.oguzhanozgokce.finishmarmarab2b.core.common
+
+object ApiRoutes {
+    const val LOGIN = "/api/Auth/user/login"
+    const val REGISTER = "/api/Auth/user/register"
+    const val GET_USER = "/api/User/{id}"
+}

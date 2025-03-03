e40911b98ebc6d7fe589b5ef773a01f3905a5718
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2025-03-03 22:07:16 +0300

    Created main viewmodel and called connection

--- app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ui/MainActivity.kt
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ui/MainActivity.kt
@@ -7,20 +7,43 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
+import com.oguzhanozgokce.finishmarmarab2b.R
+import com.oguzhanozgokce.finishmarmarab2b.core.presentation.components.FMButton
 import com.oguzhanozgokce.finishmarmarab2b.navigation.NavigationGraph
 import com.oguzhanozgokce.finishmarmarab2b.ui.splash.SplashViewModel
 import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme
 import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme.colors
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme.fontSize
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme.padding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val splashViewModel: SplashViewModel by viewModels()
+    private val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -32,15 +55,69 @@ class MainActivity : ComponentActivity() {
             splashViewModel.isSplashShow.value
         }
         setContent {
+            val uiState by mainViewModel.uiState.collectAsStateWithLifecycle()
             FMTheme {
                 val navController = rememberNavController()
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = colors.white
-                ) {
+                Box(modifier = Modifier.fillMaxSize()) {
                     NavigationGraph(navController = navController)
+
+                    if (uiState.isShowNoNetworkDialog) {
+                        NoNetworkContent(
+                            onRetryClick = { mainViewModel.onAction(MainContract.UiAction.CheckNetworkAgain) }
+                        )
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun NoNetworkContent(
+    onRetryClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colors.white),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.icon_no_connection),
+            contentDescription = null,
+            tint = colors.primary,
+            modifier = Modifier.size(padding.dimension140)
+        )
+        Spacer(modifier = Modifier.height(padding.dimension16))
+        Text(
+            text = "No Internet Connection",
+            style = FMTheme.typography.titleMediumMedium().copy(fontSize = fontSize.large)
+        )
+        Spacer(modifier = Modifier.height(padding.dimension16))
+        Text(
+            text = "Please check your internet connection and try again.",
+            style = FMTheme.typography.titleMediumMedium(),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = padding.dimension8)
+        )
+        Spacer(modifier = Modifier.height(padding.dimension16))
+        FMButton(
+            text = "Try again",
+            backgroundColor = colors.error.copy(alpha = 0.3f),
+            textColor = colors.error,
+            elevation = ButtonDefaults.buttonElevation(0.dp),
+            onClick = onRetryClick
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoNetworkContentPreview() {
+    FMTheme {
+        NoNetworkContent(
+            onRetryClick = {}
+        )
     }
 }

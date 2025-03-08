86c86b3ac6a0f2840bf4119f53e47da195d6d98c
Gamze <gamzecoskkun@gmail.com>
2025-03-08 04:07:58 +0300

    update

--- app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ecommerce/domain/usecase/product/GetCategoriesUseCase.kt
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ecommerce/domain/usecase/product/GetCategoriesUseCase.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCategoriesUseCase @Inject constructor(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
 ) {
     operator fun invoke(): Flow<PagingData<Category>> {
         return productRepository.getCategories()

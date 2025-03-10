b76951c68df93fb3258ef8fc40d98f5c4e00b752
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2025-03-10 12:44:22 +0300

    FMTopbar component created

---
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/core/presentation/components/FMTopBar.kt
@@ -0,0 +1,67 @@
+package com.oguzhanozgokce.finishmarmarab2b.core.presentation.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.oguzhanozgokce.finishmarmarab2b.R
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme.colors
+import com.oguzhanozgokce.finishmarmarab2b.ui.theme.FMTheme.typography
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FMTopBar(
+    title: String,
+    onNavigationClick: () -> Unit,
+    containerColor: Color = colors.cardBackground,
+    titleContentColor: Color = colors.background,
+    actions: (@Composable RowScope.() -> Unit)? = null
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = typography.titleMediumMedium()
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back),
+                    tint = colors.onBackground
+                )
+            }
+        },
+        actions = { actions?.let { it() } },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = containerColor,
+            titleContentColor = titleContentColor
+        ),
+        windowInsets = WindowInsets(0.dp)
+    )
+}
+
+@PreviewLightDark
+@Composable
+fun FMTopBarPreview() {
+    FMTheme {
+        FMTopBar(
+            title = "Title",
+            onNavigationClick = {}
+        )
+    }
+}
+

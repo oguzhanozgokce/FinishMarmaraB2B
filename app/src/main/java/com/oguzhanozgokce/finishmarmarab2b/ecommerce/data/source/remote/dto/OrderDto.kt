576d78c726dd3122275c6d58304ab31b7db9ba7f
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2025-03-12 16:34:36 +0300

    OrderDto and OrderRequest.kt added

---
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ecommerce/data/source/remote/dto/OrderDto.kt
@@ -0,0 +1,9 @@
+package com.oguzhanozgokce.finishmarmarab2b.ecommerce.data.source.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+
+data class OrderDto(
+    @SerializedName("id")
+    val id: Int? = null,
+)
---
+++ app/src/main/java/com/oguzhanozgokce/finishmarmarab2b/ecommerce/data/source/remote/request/OrderRequest.kt
@@ -0,0 +1,16 @@
+package com.oguzhanozgokce.finishmarmarab2b.ecommerce.data.source.remote.request
+
+import com.google.gson.annotations.SerializedName
+
+data class OrderRequest(
+    @SerializedName("user_id")
+    val userId: Int? = null,
+    @SerializedName("location_id")
+    val locationId: Int? = null,
+    @SerializedName("credit_Card_Id")
+    val creditCardId: Int? = null,
+    @SerializedName("total_Price")
+    val totalPrice: Int? = null,
+    @SerializedName("order_Status")
+    val orderStatus: String? = null
+)
